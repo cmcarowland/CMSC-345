8205490577e00302d7ef268477d26f94c410e988
Raymond Rowland <82390744+cmcarowland@users.noreply.github.com>
2025-08-09 19:10:41 +0000

    feat: Created base menu system for Bed and breakfast

---
+++ BnB/__main__.py
@@ -0,0 +1,14 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Footer, Header, Button
+
+class MainMenu(App):
+    #BINDINGS = [('d', 'toggle_dark', 'Toggle Dark Mode')]
+    TITLE = 'Bed And Breaky'
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer()
+
+if __name__ == "__main__":
+    app = MainMenu()
+    app.run()

f52e4962c8a6d35d432084e3e3ea3faee8727f16
Raymond Rowland <82390744+cmcarowland@users.noreply.github.com>
2025-08-09 12:07:16 +0000

    feat: Added css to stopwatch app for stopwatch classes

--- src/stopwatch.py
+++ src/stopwatch.py
@@ -1,12 +1,33 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header
+from textual.containers import HorizontalGroup, VerticalScroll
+from textual.widgets import Footer, Header, Button, Digits
+
+class TimeDisplay(Digits):
+    '''A widget to display time'''
+
+class StopWatch(HorizontalGroup):
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "start":
+            self.add_class("started")
+        elif event.button.id == "stop":
+            self.remove_class("started")
+
+    def compose(self):
+        yield Button('Start', id='start', variant='success')
+        yield Button('Stop', id='stop', variant='error')
+        yield Button('Reset', id='reset')
+        yield TimeDisplay("00:00:00.00")
 
 class StopWatchApp(App):
+
+    CSS_PATH = "stopwatch.css"
     BINDINGS = [('d', 'toggle_dark', 'Toggle Dark Mode')]
 
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
+        yield VerticalScroll(StopWatch(), StopWatch(), StopWatch())
 
     def action_toggle_dark(self) -> None:
         print("Called")

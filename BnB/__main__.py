da5fe3e956b137e3f5e4d550145975fe92bc0595
Raymond Rowland <82390744+cmcarowland@users.noreply.github.com>
2025-08-10 12:46:07 +0000

    refactor: Removed comment block

--- BnB/__main__.py
+++ BnB/__main__.py
@@ -15,12 +15,6 @@ class MainMenu(App):
         yield Footer()
         yield Login()
 
-    # This must be named the message class name
-    # def on_login_my_custom_message(self, message: Login.MyCustomMessage) -> None:
-    #     """Handle the custom message from the Login component."""
-    #     self.notify("It's a trap!", severity="error", timeout=10)
-    #     # self.exit()
-
 
 if __name__ == "__main__":
     app = MainMenu()

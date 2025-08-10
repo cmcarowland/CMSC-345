4226d0acd3597f2c105e96e1ef8118a0b4dd628d
Raymond Rowland <82390744+cmcarowland@users.noreply.github.com>
2025-08-10 21:18:57 +0000

    refactor: Updated login button to validate username and password

--- BnB/Login.py
+++ BnB/Login.py
@@ -1,4 +1,8 @@
+from hashlib import md5
+
+from BnB.utils import get_users, save_users
 from BnB.Create import Create
+
 from textual.screen import Screen
 from textual.app import App, ComposeResult
 from textual.containers import VerticalGroup, Grid
@@ -11,16 +15,26 @@ from textual.widgets import (
 class Login(Screen):
     CSS_PATH = 'styles.tcss'
 
+    def check_login(self) -> bool:
+        users = get_users()
+        username = self.query_one('#username').value
+        password = md5(self.query_one('#password').value.encode()).hexdigest()
+        if users.get(username) == password:
+            return True
+        
+        return False
+
     def compose(self) -> ComposeResult:
         with VerticalGroup():
             with Grid(classes="label_input"):
                 yield Label("User Name")
-                yield Input(placeholder="")
+                yield Input(id='username', placeholder="")
 
             with Grid(classes="label_input"):
                 yield Label("Password")
                 yield Input(
-                    "",
+                    id='password',
+                    placeholder="Enter Password",
                     tooltip="Obviously not your real credit card!",
                     password=True
                 )
@@ -36,3 +50,8 @@ class Login(Screen):
             self.app.exit()
         elif button_id == 'create':
             self.app.push_screen(Create())
+        elif button_id == 'login':
+            if self.check_login():
+                self.notify("Login Successful!!", severity="success", timeout=5)
+            else:
+                self.notify("Invalid username or password", severity="error", timeout=5)

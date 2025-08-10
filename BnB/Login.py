8ff663511da185384c7835ab137a34ceca54111d
Raymond Rowland <82390744+cmcarowland@users.noreply.github.com>
2025-08-10 02:05:53 +0000

    feat: Added quit button functionality

--- BnB/Login.py
+++ BnB/Login.py
@@ -1,5 +1,5 @@
-from textual.app import ComposeResult
-from textual.containers import HorizontalGroup, VerticalGroup
+from textual.app import App, ComposeResult
+from textual.containers import HorizontalGroup, VerticalGroup, Grid
 from textual.widgets import (
     Button,
     Input,
@@ -7,26 +7,36 @@ from textual.widgets import (
     MaskedInput
 )
 
+from textual.message import Message
 
 class Login(VerticalGroup):
+    CSS_PATH = 'Login.tcss'
+
+    class MyCustomMessage(Message):
+        def __init__(self, data: str) -> None:
+            super().__init__()
+            self.data = data
 
     def compose(self) -> ComposeResult:
-        yield HorizontalGroup(
-            Label("User Name"),
-            Input(placeholder="")
-        )
+        with Grid(classes="label_input"):
+            yield Label("User Name")
+            yield Input(placeholder="")
         
-        yield HorizontalGroup(
-            Label("Password"),
-            Input(
+        with Grid(classes="label_input"):
+            yield Label("Password")
+            yield Input(
                 "",
                 tooltip="Obviously not your real credit card!",
                 password=True
             )
-        )
 
-        yield HorizontalGroup(
-            Button('Login', id='login'),
-            Button('Create User', id='create'),
-            Button('Quit', id='quit')
-        )
+        with Grid(id='login_button_group'):
+            yield Button('Login', id='login')
+            yield Button('Create User', id='create')
+            yield Button('Quit', id='quit')
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        button_id = event.button.id
+        if button_id == 'quit':
+            # Use post_message to send the message; the App will receive it
+            self.post_message(self.MyCustomMessage("Data from child"))
--- BnB/__main__.py
+++ BnB/__main__.py
@@ -7,11 +7,19 @@ from textual.widgets import Footer, Header, Button
 class MainMenu(App):
     #BINDINGS = [('d', 'toggle_dark', 'Toggle Dark Mode')]
     TITLE = 'Bed And Breaky'
+    CSS_PATH = 'Login.tcss'
+
 
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
         yield Login()
+
+    def on_login_my_custom_message(self, message: Login.MyCustomMessage) -> None:
+        """Handle the custom message from the Login component."""
+        self.notify("It's a trap!", severity="error", timeout=10)
+        self.exit()
+
 
 if __name__ == "__main__":
     app = MainMenu()
